15169efa86bbc97c4e8314c9f336d1092c0a2bf6
Dileepa Jayakody <dileepajayakody@gmail.com>
2019-05-16 14:44:53 +0200

    NIMBLEFMP-674 : fixing valueCodes, codeListUri

--- src/main/java/eu/nimble/indexing/service/impl/OntologyServiceImpl.java
+++ src/main/java/eu/nimble/indexing/service/impl/OntologyServiceImpl.java
@@ -370,7 +370,7 @@ public class OntologyServiceImpl implements OntologyService {
     	// process all relevant nimble statements
     	Set<String> codeSet = new HashSet<String>();
     	// preset the codeSet with any existing
-    	codeSet.addAll(pt.getCodeList());
+    	codeSet.addAll(pt.getValueCodes());
     	// 
     	String codeListUri = null;
     	
@@ -399,9 +399,9 @@ public class OntologyServiceImpl implements OntologyService {
     		}
     	}
     	// store the codelist 
-    	pt.getCodeList().addAll(codeSet);
+    	pt.getValueCodes().addAll(codeSet);
     	// store the list uri - helpful to obtain the list of codes
-    	pt.setCodeListId(codeListUri);
+    	pt.setCodeListUri(codeListUri);
     }
 
     private Set<String> processCodedList(OntResource list) {
